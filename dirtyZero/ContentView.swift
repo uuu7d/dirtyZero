416d29e71317ef76dc5026cfc6320300182fe733
lunginspector <138542747+lunginspector@users.noreply.github.com>
2025-05-09 15:32:55 -0700

    add new ui
    enjoy the new ui

--- dirtyZero/ContentView.swift
+++ dirtyZero/ContentView.swift
@@ -16,51 +16,81 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 List {
-                    Section(header: Text("Tweaks"), content: {
-                        Button("Hide dock", action: {
-                            dirtyZeroHide(path: "/System/Library/PrivateFrameworks/CoreMaterial.framework/dockDark.materialrecipe")
-                            dirtyZeroHide(path: "/System/Library/PrivateFrameworks/CoreMaterial.framework/dockLight.materialrecipe")
-                        })
-                        Button("Hide home bar", action: {
-                            dirtyZeroHide(path: "/System/Library/PrivateFrameworks/MaterialKit.framework/Assets.car")
-                        })
-                        Button("Custom path (dangerous!)", action: {
-                            Alertinator.shared.prompt(title: "Enter custom path", placeholder: "/path/to/the/file/to/hide") { path in
-                                if let isEmpty = path, !path!.isEmpty {
-                                    dirtyZeroHide(path: path!)
-                                } else {
-                                    Alertinator.shared.alert(title: "Invalid path", body: "Enter an actual path to what you want to hide/zero.")
-                                }
-                            }
-                        })
-                    })
-                    
-                    Section(header: Text("Logs"), content: {
+                    Section {
                         HStack {
                             Spacer()
                             ZStack {
                                 LogView()
-                                    .padding(0.25)
-                                    .frame(width: 340, height: 340)
+                                    .padding(3)
+                                    .frame(width: 340, height: 260)
                             }
                             Spacer()
                         }
                         .onAppear(perform: {
                             print("[*] Welcome to dirtyZero!\n[*] Running on \(device.systemName!) \(device.systemVersion!), \(device.description)")
                         })
-                    })
-                    
-                    Section(header: Text("Actions"), content: {
-                        Button("Attempt respring", action: {
-                            // i don't think this'll work but i'm adding this from my phone so clearly i got bigger problems
-                            notify_post("com.apple.springboard.toggleLockScreen")
-                        })
-                    })
+                    } header: {
+                        HStack {
+                            Image(systemName: "terminal.fill")
+                            Text("Logs")
+                        }
+                    }
+                    Section {
+                        VStack {
+                            HStack {
+                                Button(action: {
+                                    dirtyZeroHide(path: "/System/Library/PrivateFrameworks/CoreMaterial.framework/dockDark.materialrecipe")
+                                    dirtyZeroHide(path: "/System/Library/PrivateFrameworks/CoreMaterial.framework/dockLight.materialrecipe")
+                                }) {
+                                    HStack {
+                                        Image(systemName: "dock.rectangle")
+                                        Text("Hide Dock")
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(.green.opacity(0.2))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                                
+                                Button(action: {
+                                    dirtyZeroHide(path: "/System/Library/PrivateFrameworks/MaterialKit.framework/Assets.car")
+                                }) {
+                                    HStack {
+                                        Image(systemName: "line.3.horizontal")
+                                        Text("Hide Home Bar")
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(.green.opacity(0.2))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }
+                            Button(action: {
+                                Alertinator.shared.prompt(title: "Enter custom path", placeholder: "/path/to/the/file/to/hide") { path in
+                                    if let isEmpty = path, !path!.isEmpty {
+                                        dirtyZeroHide(path: path!)
+                                    } else {
+                                        Alertinator.shared.alert(title: "Invalid path", body: "Enter an actual path to what you want to hide/zero.")
+                                    }
+                                }
+                            }) {
+                                HStack {
+                                    Image(systemName: "terminal.fill")
+                                    Text("Custom Path")
+                                }
+                                .foregroundStyle(.red)
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(.red.opacity(0.2))
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                        }
+                    } header: {
+                        HStack {
+                            Image(systemName: "hammer.fill")
+                            Text("Tweaks")
+                        }
+                    } footer: {
+                        Text("All writes are done in memory, so if something goes wrong, you can force reboot.\n\nExploit discovered by Ian Beer of Google Project Zero. Created by the jailbreak.party team.")
+                    }
                 }
-                
-                Text("Exploit discovered by Ian Beer of Google Project Zero.\nMade by Skadz.\nSpecial thanks to the jailbreak.party team.")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
             }
             .navigationTitle("dirtyZero")
         }
