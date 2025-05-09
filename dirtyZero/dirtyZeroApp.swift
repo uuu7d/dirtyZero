41b776ece07131585aaf543fbba6a7b39b5783de
Skadz <skadz@skadz.online>
2025-05-08 23:36:52 -0700

    Initial Commit

---
+++ dirtyZero/dirtyZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  dirtyZeroApp.swift
+//  dirtyZero
+//
+//  Created by Skadz on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct dirtyZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
